ec5550297454adc904433f6e4d9070e339c06bc6
Marcus Shannon <mpshannon@me.com>
2019-05-16 22:06:36 -0400

    Selectors

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Chart from "./Chart";
 import UserList from "./UserList";
+import { getLastMonth } from "./selectors";
 
 function App() {
+  const lastMonth = getLastMonth();
   return (
     <React.Fragment>
       <UserList />
--- assets/js/selectors.js
+++ assets/js/selectors.js
@@ -1,5 +1,22 @@
-function getLastMonth(rawData) {
-  return formatData(rawData);
+import { data } from "./data";
+
+export function getRawReactions() {
+  return data.raw_reactions;
+}
+
+export function getRawMessages() {
+  return data.raw_messages;
+}
+
+export function getLastMonth() {
+  return getRawMessages()
+    .map(message => {
+      return { ...message, ts: parseInt(message.ts.split(".")[0]) };
+    })
+    .filter(message => {
+      //   const currentTime = new Date().getTime() / 1000;
+      return 1546307799 - 2629743 < message.ts;
+    });
 }
 
 function formatData(data) {
