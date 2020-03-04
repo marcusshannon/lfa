ce322a6eb33239ffb190398655a8582c667ffb24
Marcus Shannon <mpshannon@me.com>
2020-03-04 00:30:41 -0500

    clean

--- lib/lfa_web/controllers/event_controller.ex
+++ lib/lfa_web/controllers/event_controller.ex
@@ -6,7 +6,7 @@ defmodule LFAWeb.EventController do
     text(conn, challenge)
   end
 
-  def event(conn, %{"event" => %{"item" => %{"ts" => ts}}} = params) do
+  def event(conn, %{"event" => %{"item" => %{"ts" => ts}}}) do
     Slack.update_reactions(ts)
     send_resp(conn, 200, "ok")
   end
